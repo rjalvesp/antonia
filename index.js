2ae22acced0ae2a359ec41733c070779f6183a5b
Rj <me@fibonaxis.io>
2021-08-05 21:37:02 -0500

    qwe

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-require('dotenv').config(); //initialize dotenv
-const R = require('ramda');
-const replies = require('./replies');
-const Discord = require('discord.js');
-const { isMessageFromBot, isBotMentioned, pickCommand } = require('./utils/common');
+try {
+  require("dotenv").config(); //initialize dotenv
+} catch (e) {
+} finally {
+  const replies = require("./replies");
+  const Discord = require("discord.js");
+  const {
+    isMessageFromBot,
+    isBotMentioned,
+    pickCommand,
+  } = require("./utils/common");
 
-const client = new Discord.Client(); //create new client
+  const client = new Discord.Client(); //create new client
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
 
-client.login(process.env.CLIENT_TOKEN);
+  client.login(process.env.CLIENT_TOKEN);
 
-client.on('message', async msg => {
-  if (isMessageFromBot(msg)) {
-    return;
-  }
-  if (isBotMentioned(msg)) {
-    replies['!antonia'](msg);
-    return;
-  }
-  const fn = replies[pickCommand(msg)];
-  
-  if (!fn) return;
-  return fn(msg);
-});
+  client.on("message", async (msg) => {
+    if (isMessageFromBot(msg)) {
+      return;
+    }
+    if (isBotMentioned(msg)) {
+      replies["!antonia"](msg);
+      return;
+    }
+    const fn = replies[pickCommand(msg)];
+
+    if (!fn) return;
+    return fn(msg);
+  });
+}
